f3df92d68e16c6e27faaec14fcb217525e749a43
glonpl <bratajczyk@o2.pl>
2019-05-23 01:13:51 +0200

    First test, but headles doesnt work
    First test, but headles doesnt work

--- src/test/java/SwapiTest.java
+++ src/test/java/SwapiTest.java
@@ -1,28 +1,38 @@
 
 //Testing https://swapi.co/
 import com.gargoylesoftware.htmlunit.BrowserVersion;
-import org.junit.Assert;
-import org.junit.Ignore;
-import org.junit.Test;
-import org.openqa.selenium.By;
+
+
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 public class SwapiTest {
-
+    String driverPath = "C:\\Users\\Glon\\Desktop\\Nowy folder\\Nowy folder\\geckodriver.exe";
 
     @Test
     public void CanWeGetMainGUIPage(){
-        WebDriver driver = new HtmlUnitDriver(BrowserVersion.BEST_SUPPORTED,true);
+        WebDriver driver;
+        System.setProperty("webdriver.gecko.driver", driverPath);
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("marionette", true);
+        driver = new FirefoxDriver(capabilities);
+        driver.get("https://swapi.co/");
+        assertTrue(driver.getTitle().contains("Star Wars"));
+        driver.quit();
+    }
+    @Test
+    public void CanWeGetMainGUIPageHeadless(){
+        WebDriver driver;
 
 
-
-
-
+        driver = new HtmlUnitDriver();
+        driver.get("https://swapi.co/");
+        assertTrue(driver.getTitle().contains("Star Wars"));
+        driver.quit();
     }
 }
