9b2019e666e5d63a2d6542c1c1b850bbaf5fb02b
glonpl <bratajczyk@o2.pl>
2019-05-22 23:00:19 +0200

    Let's Start

---
+++ src/test/java/SwapiTest.java
@@ -0,0 +1,18 @@
+
+//Testing https://swapi.co/
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SwapiTest {
+
+}
