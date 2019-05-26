33eff9b4b3c27a9dc9c4c83882dfc4eb3e63901f
glonpl <bratajczyk@o2.pl>
2019-05-26 20:50:35 +0200

    Let's Crud Authors

---
+++ src/test/java/HeadlessTest.java
@@ -0,0 +1,264 @@
+import Pages.BooksPage;
+import Pages.LoginPage;
+import io.github.bonigarcia.SeleniumExtension;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SeleniumExtension.class)
+public class HeadlessTest {
+    WebDriverWait wait;
+    private WebDriver driver;
+    private WebDriver driver2;
+
+    public HeadlessTest() {
+        this.driver = new HtmlUnitDriver();
+        wait = new WebDriverWait(driver, 10);
+        System.setProperty("webdriver.gecko.driver", "resources/geckodriver.exe");
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+
+        this.driver2 = new FirefoxDriver(options);
+    }
+
+    @BeforeEach
+    public void Setup() {
+        //  driver.get("http://bookcatalog.azurewebsites.net/Account/Login");
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        LoginPage loginPage2 = PageFactory.initElements(driver2, LoginPage.class);
+        loginPage2.logIn("admin@admin.com", "admin1");
+    }
+
+    @AfterEach
+    public void Cleanup() {
+        driver.quit();
+        driver2.quit();
+    }
+
+    @Test
+    public void AreWeLoggedInAsAdminHeadless() {
+        new WebDriverWait(driver, 5);
+        assertTrue(driver.findElement(By.id("UserName")).getText().contains("admin"));
+    }
+
+
+    @Test
+    public void AreWeLoggedInAsAdminHeadlessFirefox() {
+
+        new WebDriverWait(driver2, 5);
+        assertTrue(driver2.findElement(By.id("UserName")).getText().contains("admin"));
+    }
+
+    @Test
+    public void CanWeCountBooksHeadless() {
+
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        new WebDriverWait(driver, 5);
+        if (!loginPage.UsernameLoggedin.contains("admin")) {
+            assertSame("Zalogowany Admin", "Niezalogowany");
+        } else {
+            loginPage.bookPage();
+            new WebDriverWait(driver, 5);
+            BooksPage books = PageFactory.initElements(driver, BooksPage.class);
+            new WebDriverWait(driver, 5);
+            assertSame(5, books.howManyBooks(), "Czy jest 5 książek?");
+        }
+    }
+
+    @Test
+    public void CanWeCountBooksHeadlessFirefox() {
+
+        LoginPage loginPage = PageFactory.initElements(driver2, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        new WebDriverWait(driver2, 5);
+        if (!loginPage.UsernameLoggedin.contains("admin")) {
+            assertSame("Zalogowany Admin", "Niezalogowany");
+        } else {
+            loginPage.bookPage();
+            new WebDriverWait(driver2, 5);
+            BooksPage books = PageFactory.initElements(driver2, BooksPage.class);
+            new WebDriverWait(driver2, 5);
+            assertSame(5, books.howManyBooks(), "Czy jest 5 książek?");
+        }
+    }
+//    @Test
+//    public void CanWeAddBooksHeadlessFirefox() {
+//
+//        LoginPage loginPage = PageFactory.initElements(driver2, LoginPage.class);
+//        loginPage.logIn("admin@admin.com", "admin1");
+//        new WebDriverWait(driver2, 5);
+//        if (!loginPage.UsernameLoggedin.contains("admin")) {
+//            assertSame("Zalogowany Admin", "Niezalogowany");
+//        } else {
+//            loginPage.bookPage();
+//            new WebDriverWait(driver2, 5);
+//            BooksPage books = PageFactory.initElements(driver2, BooksPage.class);
+//            new WebDriverWait(driver2, 5);
+//            books.toCreate();
+//            new WebDriverWait(driver2, 5);
+//            books = PageFactory.initElements(driver2, BooksPage.class);
+//            new WebDriverWait(driver2, 5);
+//            books.createBook("Artur");
+//        }
+//    }
+//
+
+    @Test
+    public void CanWeGetToCreatePageHeadless() {
+        LoginPage loginPage = PageFactory.initElements(driver2, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        new WebDriverWait(driver2, 5);
+        if (!loginPage.UsernameLoggedin.contains("admin")) {
+            assertSame("Zalogowany Admin", "Niezalogowany");
+        } else {
+            loginPage.bookPage();
+            new WebDriverWait(driver2, 5);
+            BooksPage books = PageFactory.initElements(driver2, BooksPage.class);
+            books.toCreate();
+            assertTrue(driver2.getTitle().contains("Create"));
+        }
+    }
+//Nie działa, bo Datepicker narzeka
+
+    @Test
+    public void CanWeGetToCreatePageHeadlessFirefox() {
+        LoginPage loginPage = PageFactory.initElements(driver2, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        new WebDriverWait(driver2, 5);
+        if (!loginPage.UsernameLoggedin.contains("admin")) {
+            assertSame("Zalogowany Admin", "Niezalogowany");
+        } else {
+            loginPage.bookPage();
+            new WebDriverWait(driver2, 5);
+            BooksPage books = PageFactory.initElements(driver2, BooksPage.class);
+            // List<WebElement> ksiazki =books.getBooks();
+            //  int beforeAdd = ksiazki.size();
+            books.toCreate();
+            assertTrue(driver2.getTitle().contains("Create"));
+//            books = PageFactory.initElements(driver2, BooksPage.class);
+//            books.createBook("Testowanie", "300", "01012013", "30", "1");
+//            books.toBooks();
+//            books = PageFactory.initElements(driver2, BooksPage.class);
+//            new WebDriverWait(driver2, 5);
+//
+//            if(!(beforeAdd+1==books.howManyBooks())){
+//                assertSame("Jest o jedna ksiazke wiecej","Nie jest","Add Nie dziala");
+//            }else{
+//             books.deleteLastBook();
+//              if(!(beforeAdd==books.howManyBooks())){
+//                 assertSame("Jest tyle samo ksiazek co na poczatku","Nie jest","Delete Nie dziala");
+//
+//             }
+//            }
+        }
+//        assertTrue(true);
+    }
+
+    @Test
+    public void CanWeGetSearchHeadless() {
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        new WebDriverWait(driver, 5);
+        if (!loginPage.UsernameLoggedin.contains("admin")) {
+            assertSame("Zalogowany Admin", "Niezalogowany");
+        } else {
+
+            loginPage.bookPage();
+            new WebDriverWait(driver, 5);
+            BooksPage books = PageFactory.initElements(driver, BooksPage.class);
+            books.searchBooks("Krz");
+            assertEquals(1, books.howManyBooks());
+        }
+
+    }
+
+    @Disabled
+    @Test
+    public void CanWeGetSearchHeadlessFirefox() {
+        LoginPage loginPage = PageFactory.initElements(driver2, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        new WebDriverWait(driver2, 5);
+        if (!loginPage.UsernameLoggedin.contains("admin")) {
+            assertSame("Zalogowany Admin", "Niezalogowany");
+        } else {
+
+            loginPage.bookPage();
+            new WebDriverWait(driver2, 5);
+            BooksPage books = PageFactory.initElements(driver2, BooksPage.class);
+            books.searchBooks("Krz");
+            assertEquals(1, books.howManyBooks());//co ciekawe wyświetla tu 5(czyli wszystkie)
+        }
+
+    }
+
+
+    @Test
+    public void CanWeGetBookByIdHeadlessFirefox() {
+        LoginPage loginPage = PageFactory.initElements(driver2, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        new WebDriverWait(driver2, 5);
+        if (!loginPage.UsernameLoggedin.contains("admin")) {
+            assertSame("Zalogowany Admin", "Niezalogowany");
+        } else {
+
+            loginPage.bookPage();
+            new WebDriverWait(driver2, 5);
+            BooksPage books = PageFactory.initElements(driver2, BooksPage.class);
+
+            assertTrue(books.getBook(1).contains("Quo Vadis"));
+        }
+
+    }
+
+    @Test
+    public void CanWeGetBookByIdHeadless() {
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        new WebDriverWait(driver, 5);
+        if (!loginPage.UsernameLoggedin.contains("admin")) {
+            assertSame("Zalogowany Admin", "Niezalogowany");
+        } else {
+
+            loginPage.bookPage();
+            new WebDriverWait(driver, 5);
+            BooksPage books = PageFactory.initElements(driver, BooksPage.class);
+
+            assertTrue(books.getBook(1).contains("Quo Vadis"));
+        }
+
+    }
+
+    @Test
+    public void CanWeGetLastBookHeadlessFirefox() {
+        LoginPage loginPage = PageFactory.initElements(driver2, LoginPage.class);
+        loginPage.logIn("admin@admin.com", "admin1");
+        new WebDriverWait(driver2, 5);
+        if (!loginPage.UsernameLoggedin.contains("admin")) {
+            assertSame("Zalogowany Admin", "Niezalogowany");
+        } else {
+
+            loginPage.bookPage();
+            new WebDriverWait(driver2, 5);
+            BooksPage books = PageFactory.initElements(driver2, BooksPage.class);
+            books.searchBooks("Krz");
+            assertEquals(1, books.getLastBook());//co ciekawe wyświetla tu 5(czyli wszystkie)
+        }
+
+    }
+
+
+}
---
+++ src/test/java/Pages/AuthorsPage.java
@@ -0,0 +1,99 @@
+package Pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class AuthorsPage {
+    public WebDriver driver;
+
+    @FindBy(name = "Title")
+    private WebElement title;
+
+    @FindBy(id = "Length")
+    private WebElement length;
+
+    @FindBy(id = "ReleaseDate")
+    private WebElement releaseDate;
+
+    @FindBy(name = "Price")
+    private WebElement price;
+
+    @FindBy(id = "AuthorId")
+    private WebElement authorID;
+
+    @FindBy(id = "BookId")
+    private WebElement BookId;
+
+    @FindBy(name = "SearchString")
+    private WebElement searchBar;
+
+
+    @FindBy(xpath = "//table[@class='table']/tbody/tr")
+    private List<WebElement> tableContent;
+
+
+    public AuthorsPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void toCreate(){
+        driver.get("http://bookcatalog.azurewebsites.net/Books/Create");
+
+    }
+
+    public void createBook(String title, String length, String date, String price, String authorID){
+        this.title.sendKeys(title);
+        this.length.sendKeys(length);
+        this.releaseDate.sendKeys(date);
+        this.releaseDate.sendKeys(date);
+        this.price.sendKeys(price);
+        this.authorID.sendKeys(authorID);
+        this.authorID.submit();
+    }
+
+    public void searchBooks(String searchPhrase){
+        searchBar.sendKeys(searchPhrase);
+        searchBar.submit();
+    }
+
+    public String getBook(int id){
+        WebElement tr = driver.findElement(By.xpath("(//table[@class='table']/tbody/tr)[" + id + "]"));
+        return tr.getText();
+    }
+
+    public void editBook(int id, String title, String length, String date, String price, String authorID){
+        driver.get("http://bookcatalog.azurewebsites.net/Authors/Edit/" + id);
+        this.title.clear();
+        this.title.sendKeys(title);
+
+        this.length.clear();
+        this.length.sendKeys(length);
+
+        this.releaseDate.clear();
+        this.releaseDate.sendKeys(date);
+
+        this.price.clear();
+        this.price.sendKeys(price);
+
+        this.authorID.clear();
+        this.authorID.sendKeys(authorID);
+
+        this.authorID.submit();
+    }
+
+    public int howManyBooks(){
+        return tableContent.size();
+    }
+
+    public List<WebElement> getBooks(){
+        return tableContent;
+    }
+
+
+    public String getLastBook() {
+        WebElement tr = driver.findElement(By.xpath("(//table[@class='table']/tbody/tr)[last()]"));
+        return tr.getText();
+    }
